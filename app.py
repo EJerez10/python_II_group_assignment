b3a44a8efcd9241f93c1b921464a61487fc0ff18
EJerez10 <enzoj36@gmail.com>
2026-03-21 18:03:46 +0100

    title changes

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Raviolution", page_icon="📈", layout="wide")
 
-home = st.Page("pages/home.py", title="Home", icon="🏠")
-team = st.Page("pages/1_👥_Team.py", title="Team", icon="👥")
-watchlist = st.Page("pages/2_👀_Watchlist.py", title="Watchlist", icon="👀")
-overview = st.Page("pages/3_📊_Overview.py", title="Stock Overview", icon="📊")
-recommendations = st.Page("pages/4_🤖_Recommendations.py", title="Recommendations", icon="🤖")
+home = st.Page("pages/0_Home.py", title="Home", icon="🏠")
+team = st.Page("pages/1_Team.py", title="Team", icon="👥")
+watchlist = st.Page("pages/2_Watchlist.py", title="Watchlist", icon="👀")
+overview = st.Page("pages/3_Overview.py", title="Stock Overview", icon="📊")
+recommendations = st.Page("pages/4_Recommendations.py", title="Recommendations", icon="🤖")
 
 pg = st.navigation([home, team, watchlist, overview, recommendations])
 pg.run()
